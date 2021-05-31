c331d9b4a6f7babfb1065663a37527e9f69fa32b
ebarbin <ebarbin@geocom.com.uy>
2021-05-31 13:20:48 -0300

    changes

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const moment = require('moment');
 const discordBot = new Discord.Client();
 const INPUT_CHANNEL_NAME = 'admin-new-players';
 const OUTPUT_CHANNEL_NAME = 'admin-new-players-csv';
-const AUTHOR_NAME = 'Xander646'; //'TEC1 Bot';
+const AUTHOR_NAME = 'TEC1 Bot'; //'TEC1 Bot';
 
 let outPutChannel = null;
 
@@ -18,18 +18,37 @@ discordBot.on('ready', () => {
 });
 
 discordBot.on('message', (message) => {
-    if (message.author.username != AUTHOR_NAME) return;
+    //if (message.author.username != AUTHOR_NAME) return;
     message.channel.fetch().then(channel => { 
         if (channel.name == INPUT_CHANNEL_NAME) {
             try {
-                const splittedMessage = message.content.split("\n");
-            
-                const steamNameRaw = splittedMessage[0].split(":")[1].trim();
-                const scumNameRaw = splittedMessage[1].split(":")[1].trim();
-                const steamIdRaw = splittedMessage[4].split(":")[1].trim();
-    
-                const outPutMessage = moment().format('DD/MM/yyyy') + ';' + steamNameRaw + ';' + scumNameRaw + ';' + steamIdRaw;
-                outPutChannel.send(outPutMessage);
+
+
+
+                if (message.content) {
+                    
+                    const splittedMessage = message.content.split("\n");
+                    console.log(splittedMessage);
+                    if (splittedMessage.length > 0) {
+
+                        if (splittedMessage.length == 11) {
+                            const steamNameRaw = splittedMessage[1].split(":")[1];
+                            const scumNameRaw = splittedMessage[2].split(":")[1];
+                            const steamIdRaw = splittedMessage[5].split(":")[1];
+                
+                            const outPutMessage = "```" + moment().format('DD/MM/yyyy') + ';' + steamNameRaw + ';' + scumNameRaw + ';' + steamIdRaw + "```";
+                            outPutChannel.send(outPutMessage);
+                        } else {
+                            const steamNameRaw = splittedMessage[0].split(":")[1];
+                            const scumNameRaw = splittedMessage[1].split(":")[1];
+                            const steamIdRaw = splittedMessage[4].split(":")[1];
+                
+                            const outPutMessage = "```" + moment().format('DD/MM/yyyy') + ';' + steamNameRaw + ';' + scumNameRaw + ';' + steamIdRaw + "```";
+                            outPutChannel.send(outPutMessage);
+                        }
+
+                    }
+                }
             } catch(error) {
                 console.log(error);
             }
